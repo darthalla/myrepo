a4f9039fece1b9a446a13d324f1e2d09dec9c106
Alla DH <90313660+darthalla@users.noreply.github.com>
2021-09-15 19:10:31 +0300

    adding my code
    this is the first version of my homework

---
+++ homework.py
@@ -0,0 +1,30 @@
+action = input('Я могу из любого числа сделать 4. Показать, как я это делаю? ')
+print('Введите число')
+
+typenumber = input()
+print ()
+a = int(typenumber)
+b = str(a)
+c = str(a*2)
+print(typenumber + 'х2=' + c)
+
+number2 = a*2
+d = int(number2)
+e = str(d)
+f = str(number2+8)
+print(e + '+8=' + f)
+
+
+number3 = number2+8
+g = int(number3)
+h = str(g)
+f2 = str(g/2)
+print(h + '/2=' + f2)
+
+
+number4 = number3/2
+i = int(number4)
+j = str(i)
+f3 = str(number4-a)
+print(j + '-' + typenumber + '=' + f3)
+
